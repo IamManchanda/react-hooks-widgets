904eb681669bd20ec978a1161e1af2c7de6e253f
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-02 22:37:37 +0530

    Expanding the Accordion!

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -1,19 +1,26 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Accordion = ({ items }) => {
+  const [activeIdx, setActiveIdx] = useState(null);
+
   const handleTitleClick = (idx) => {
-    console.log("Title clicked", idx);
+    setActiveIdx(idx);
   };
 
   return (
     <div className="ui styled accordion">
       {items.map(({ id, title, content }, idx) => (
         <Fragment key={id}>
-          <div className="title active" onClick={() => handleTitleClick(idx)}>
+          <div
+            className={`title${idx === activeIdx ? " active" : ""}`}
+            onClick={() => handleTitleClick(idx)}
+          >
             <i className="dropdown icon"></i>
             {title}
           </div>
-          <div className="content active">{content}</div>
+          <div className={`content${idx === activeIdx ? " active" : ""}`}>
+            {content}
+          </div>
         </Fragment>
       ))}
     </div>
