6f860dd7c8e27d9b8d3173dec2144008d1471317
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-02 22:01:04 +0530

    handleTitleClick function!

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -1,17 +1,23 @@
 import React, { Fragment } from "react";
 
-const Accordion = ({ items }) => (
-  <div className="ui styled accordion">
-    {items.map(({ id, title, content }) => (
-      <Fragment key={id}>
-        <div className="title active">
-          <i className="dropdown icon"></i>
-          {title}
-        </div>
-        <div className="content active">{content}</div>
-      </Fragment>
-    ))}
-  </div>
-);
+const Accordion = ({ items }) => {
+  const handleTitleClick = (idx) => {
+    console.log("Title clicked", idx);
+  };
+
+  return (
+    <div className="ui styled accordion">
+      {items.map(({ id, title, content }, idx) => (
+        <Fragment key={id}>
+          <div className="title active" onClick={() => handleTitleClick(idx)}>
+            <i className="dropdown icon"></i>
+            {title}
+          </div>
+          <div className="content active">{content}</div>
+        </Fragment>
+      ))}
+    </div>
+  );
+};
 
 export default Accordion;
