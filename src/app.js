2ce597e9fb88fc1a654c1252bc7297c196ca56a2
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-03 20:08:28 +0530

    More working progress in dropdown component!

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 import Accordion from "./components/accordion";
 import Counter from "./components/counter";
@@ -9,22 +9,28 @@ import Dropdown from "./components/dropdown";
 import items from "./fixtures/items";
 import options from "./fixtures/options";
 
-const App = () => (
-  <Fragment>
-    <h1>Widgets App</h1>
-    <h2>Accordion</h2>
-    <Accordion items={items} />
-    <h2>Counter</h2>
-    <Counter />
-    <h2>Search List</h2>
-    <Search initialSearchTerm="" />
-    <h2>Dropdown</h2>
-    <Dropdown
-      label="Select Color"
-      placeholder="Please select a Color"
-      options={options}
-    />
-  </Fragment>
-);
+const App = () => {
+  const [selectedOption, setSelectedOption] = useState({});
+  const handleSelectedChange = (option) => setSelectedOption(option);
+  return (
+    <Fragment>
+      <h1>Widgets App</h1>
+      <h2>Accordion</h2>
+      <Accordion items={items} />
+      <h2>Counter</h2>
+      <Counter />
+      <h2>Search List</h2>
+      <Search initialSearchTerm="" />
+      <h2>Dropdown</h2>
+      <Dropdown
+        label="Select Color"
+        placeholder="Please select a Color"
+        options={options}
+        selected={selectedOption}
+        onSelectedChange={handleSelectedChange}
+      />
+    </Fragment>
+  );
+};
 
 export default App;
--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import isEmptyObject from "../utils/is-empty-object";
 
-const Dropdown = ({ label = "", placeholder = "", options = [] }) => {
+const Dropdown = ({
+  label = "",
+  placeholder = "",
+  options = [],
+  selected = {},
+  onSelectedChange = () => {},
+}) => {
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const handleDropdownClick = () => setOpenDropdown(!openDropdown);
   return (
-    <div className="ui form">
+    <div className="ui form" style={{ width: "600px" }}>
       <div className="field">
         <label className="label">{label}</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          className={`ui selection dropdown${
+            openDropdown ? " visible active" : ""
+          }`}
+          onClick={handleDropdownClick}
+        >
           <i className="dropdown icon"></i>
-          <div className="text">{placeholder}</div>
-          <div className="menu visible transition">
+          <div className="text">
+            {isEmptyObject(selected) ? placeholder : selected.label}
+          </div>
+          <div className={`menu${openDropdown ? " visible transition" : ""}`}>
             {options.map(({ id, label, value }) => (
-              <div key={id} className="item">
+              <div
+                key={id}
+                className={`item${
+                  value === selected.value ? " active selected" : ""
+                }`}
+                onClick={() => onSelectedChange({ id, label, value })}
+              >
                 {label}
               </div>
             ))}
