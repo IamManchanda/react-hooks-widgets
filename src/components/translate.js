078dfc44fbb8dc9eb6a798d0b295928d553b4eb2
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-03 23:59:34 +0530

    more fine grain in translate!

--- src/components/translate.js
+++ src/components/translate.js
@@ -1,18 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import Dropdown from "./dropdown";
 
 import languageOptions from "../fixtures/language-options";
+import isEmptyObject from "../utils/is-empty-object";
 
 const Translate = () => {
   const [selectedLanguageOption, setSelectedLanguageOption] = useState({});
   return (
-    <Dropdown
-      label="Select Language"
-      placeholder="Please select a Language"
-      options={languageOptions}
-      selected={selectedLanguageOption}
-      onSelectedChange={setSelectedLanguageOption}
-    />
+    <Fragment>
+      <Dropdown
+        label="Select Language"
+        placeholder="Please select a Language"
+        options={languageOptions}
+        selected={selectedLanguageOption}
+        onSelectedChange={setSelectedLanguageOption}
+      />
+      {!isEmptyObject(selectedLanguageOption) && (
+        <div className="ui segment" style={{ width: "600px" }}>
+          <p>{selectedLanguageOption.label}</p>
+        </div>
+      )}
+    </Fragment>
   );
 };
 
